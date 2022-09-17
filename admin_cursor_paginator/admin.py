10d6e6d4edeee9b9377e37d828f138e87e7d7824
Ilya Melnitskiy <melnitskiy_i_m@onyx-team.com>
2022-09-17 21:47:23 +0300

    Add support for django 4.1 and bump version
    Signed-off-by: Ilya Melnitskiy <melnitskiy_i_m@onyx-team.com>

--- admin_cursor_paginator/admin.py
+++ admin_cursor_paginator/admin.py
@@ -1,3 +1,5 @@
+import django
+
 from django.contrib import admin
 
 from .paginator import AdminCursorPaginator
@@ -16,6 +18,13 @@ class CursorPaginatorAdmin(admin.ModelAdmin):
     show_query_result_count = True
 
     def get_paginator(self, request, queryset, per_page, orphans=0, allow_empty_first_page=True):
+        # django built-in `history_view` shares `ModelAdmin` paginator,
+        # so we need to pass original one for this specific case
+        if django.VERSION >= (4, 1):
+            from django.contrib.admin.models import LogEntry
+            if queryset.model is LogEntry:
+                return self.paginator(queryset, per_page, orphans, allow_empty_first_page)
+
         return AdminCursorPaginator(request, queryset, per_page, self.cursor_ordering_field)
 
     def get_changelist(self, request, **kwargs):
