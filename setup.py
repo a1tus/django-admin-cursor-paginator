0e3df2d3611f623d05f5520d1ac96da882b51ad2
Ilya Melnitskiy <melnitskiy_i_m@onyx-team.com>
2023-07-09 18:11:54 +0400

    Add support for django 4.2 and python 3.11 and bump version
    Signed-off-by: Ilya Melnitskiy <melnitskiy_i_m@onyx-team.com>

--- setup.py
+++ setup.py
@@ -3,4 +3,20 @@
 from setuptools import setup
 
 
-setup()
+def read(rel_path: str) -> str:
+    here = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(here, rel_path)) as fp:
+        return fp.read()
+
+
+def get_version(rel_path: str) -> str:
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):  # __version__ = '0.1.2'
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    raise RuntimeError('Unable to find version string.')
+
+
+setup(
+    version=get_version('admin_cursor_paginator/version.py'),
+)
