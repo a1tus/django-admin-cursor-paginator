5cef97ec80c3c8ff0a7597bdea4bc6a861bcbdf9
Ilya Melnitskiy <melnitskiy_i_m@onyx-team.com>
2023-12-26 01:08:13 +0400

    Fix model pk warning for test project
    Signed-off-by: Ilya Melnitskiy <melnitskiy_i_m@onyx-team.com>

--- testproject/catalog/models.py
+++ testproject/catalog/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 
 class Product(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
